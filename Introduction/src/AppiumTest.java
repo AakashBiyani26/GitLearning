28fbdbf1d47fd74d89d6442bc5169663da32e720
aakash <aakashkbiyani@gmail.com>
2021-10-07 01:37:41 +0530

    Architect asian dev

---
+++ Introduction/src/AppiumTest.java
@@ -0,0 +1,12 @@
+
+public class AppiumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3;
+		System.out.println("notice period is "+a);
+
+	}
+
+}
